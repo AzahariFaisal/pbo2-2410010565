bea6e6827db277256b04696b563a8ec02bcf4c74
Azahari Faisal (2410010565) <2410010565@uniska-bjm.ac.id>
2026-09-23 21:05:45 +0800

    Menambahkan HaloPBO2 dan KartuMahasiswa

---
+++ p01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/KartuMahasiswa.java
@@ -0,0 +1,17 @@
+package id.ac.uniska.pbo2.p01;
+
+public class KartuMahasiswa {
+
+    public static void main(String[] args) {
+        System.out.println("================================");
+        System.out.println("       KARTU MAHASISWA PBO 2");
+        System.out.println("================================");
+        System.out.println("Nama     : Azahari Faisal");
+        System.out.println("NPM      : 2410010565");
+        System.out.println("Prodi    : Teknik Informatika");
+        System.out.println("Semester : 5");
+        System.out.println("Alasan   : Ingin memahami konsep");
+        System.out.println("           pemrograman berbasis objek.");
+        System.out.println("================================");
+    }
+}
